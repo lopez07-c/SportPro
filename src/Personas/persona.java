c01d4ebf4b894985ec4eb469e0896eb62c3379ba
crmora <crmora@est.utn.ac.cr>
2026-06-25 14:19:32 -0600

    creacion de paquete
    clase persona y sus atributos

--- src/Personas/persona.java
+++ src/Personas/persona.java
@@ -4,10 +4,54 @@
  */
 package Personas;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author UTN
  */
 public class persona {
+    protected String nombre;
+    protected int identificación;
+    protected String nacionalidad;
+    protected LocalDate fechaNaci;
+    protected int telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getIdentificación() {
+        return identificación;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public LocalDate getFechaNaci() {
+        return fechaNaci;
+    }
+
+    public String getTelefono() {
+        return telefono;
+        
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
+    
+    public LocalDate getFecha(){
+        return this.fechaNaci;
+    }
+    public int calcularEdad(){
+        LocalDate hoy =LocalDate.now();
+        return java.time.Period.between(fechaNaci, hoy).getYears();
+    }
+      @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", nacionalidad=" + nacionalidad + ", fechaNaci=" + fechaNaci + '}';
+    }
 }
